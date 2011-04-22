8fd06b2b593a90d9eb93187fd272151e786c3c54
Chris Gonzales <cgonzo86@gmail.com>
2011-04-22 18:09:26 -0400

    fix preprocessor

--- cover_song_preprocess.py
+++ cover_song_preprocess.py
@@ -8,10 +8,10 @@ import sys
 old_line=''
 for line in sys.stdin:
 	stripped_line=line.rstrip()
-	if(line[0]=='%'):
+	if(stripped_line[0]=='%'):
 		print old_line+'\n'
-		old_line=''
-	else
+		old_line=stripped_line
+	else:
 		old_line+='\t'+stripped_line
 print old_line+'\n'
 
