339bd574ecf4f0e7976ffe8a944ad9946a7256ca
Chris Gonzales <cgonzo86@gmail.com>
2011-04-23 11:20:09 -0700

    works without hadoop

--- average_tempo.py
+++ average_tempo.py
@@ -7,15 +7,16 @@ import hdf5_getters
 # output: artist_id artist_name
 def map(line):
 	h5 = hdf5_getters.open_h5_file_read(line)
-	tempo=hdf5_getters.get_tempo(h5,0)
-	yield("1",tempo)
+	if(h5):
+		tempo=hdf5_getters.get_tempo(h5,0)
+		yield("1",str(tempo))
 
 def reduce(word, counts):
 	tempo_total=0
 	for count in counts:
-		tempo_total+=count
-	tempo_total/=len(counts)
-	
+		tempo_total+=float(count)
+	tempo_average=tempo_total/len(counts)
+	yield("average",str(tempo_average))
 
 if __name__ == "__main__":
   common.main(map, reduce)
