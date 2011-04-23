a1cc24cf920e249939c3728ee0d2cfd2541e699c
Chris Gonzales <cgonzo86@gmail.com>
2011-04-23 10:12:55 -0700

    cover_song3 works

--- cover_song.sh
+++ cover_song.sh
@@ -2,3 +2,5 @@ hadoop fs -rmr cover_song1
 hadoop jar $SJAR -mapper "/usr/bin/python cover_song1.py map" -reducer "/usr/bin/python cover_song1.py reduce" -input cover_songs.txt -input tracks_per_year.txt -output cover_song1 -file "cover_song1.py" -file "common.py"
 hadoop fs -rmr cover_song2
 hadoop jar $SJAR -mapper "/usr/bin/python cover_song2.py map" -reducer "/usr/bin/python cover_song2.py reduce" -input cover_song1 -output cover_song2 -file "cover_song2.py" -file "common.py"
+hadoop fs -rmr cover_song3
+hadoop jar $SJAR -mapper "/usr/bin/python cover_song3.py map" -reducer "/usr/bin/python cover_song3.py reduce" -input cover_song2 -output cover_song3 -file "cover_song3.py" -file "common.py"
--- cover_song3.py
+++ cover_song3.py
@@ -2,19 +2,23 @@
 
 import common
 import re
+import json
 
 # mapper 
 # none
 def map(line):
-	pass
-
+	if(line):
+		line_split=re.split("\t",line)
+		yield(line_split[0],line_split[1])
 # reducer
 # input
 # newer artist ID: oldest artist ID
 # output
 # artist: artists covered
 def reduce(word, counts):
-	yield(word,json.dumps(counts)
+	pagerank=1.0
+	data_to_output=[pagerank, counts]
+	yield(word,json.dumps(data_to_output))
 
 if __name__ == "__main__":
   common.main(map, reduce)
