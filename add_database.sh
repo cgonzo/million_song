333928c62e9e3dd35e7906eb93af2d7b92570ca8
Chris Gonzales <cgonzo86@gmail.com>
2011-05-03 20:04:41 +0000

    use filelist.txt in /mnt/data

--- add_database.sh
+++ add_database.sh
@@ -1,4 +1,4 @@
 hadoop fs -rm filelist.txt
-hadoop fs -copyFromLocal filelist.txt filelist.txt
+hadoop fs -copyFromLocal /mnt/data/filelist.txt filelist.txt
 hadoop fs -rmr data
 hadoop jar $SJAR -D mapred.map.tasks=4 -mapper "/usr/bin/python add_database.py map" -reducer NONE -input filelist.txt -output data -cmdenv LD_LIBRARY_PATH=$LD_LIBRARY_PATH -file "add_database.py" -file "common.py" -file "classifiers_base.py" -file "hdf5_getters.py"
