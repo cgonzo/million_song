58a088132d0e33e3b557d2ef902ad037de8800c5
Chris Gonzales <cgonzo86@gmail.com>
2011-05-04 14:08:38 -0400

    modify loudness to use hadoop data

--- loudness.py
+++ loudness.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python
 
+# find song with highest variance in loudness
+
 import common
 import hdf5_getters
 
 # input: file name
 # output: artist_id artist_name
 def map(line):
-	h5 = hdf5_getters.open_h5_file_read(line)
-	if(h5):
-		loudness=hdf5_getters.get_segments_loudness_start(h5,0)
-		if len(loudness)>0:
-			average_loudness=sum(loudness)/len(loudness)
-			variance=0
-			for value in loudness:
-				variance+=(value-average_loudness)**2
-			variance=variance/len(loudness)
-			track_id=hdf5_getters.get_title(h5,0)
-			artist_name=hdf5_getters.get_artist_name(h5,0)
-			variance_string="%010.6f" %variance
-			#variance_string="%f" %variance
-			yield(variance_string,artist_name+" -- "+track_id)
-		h5.close()
+	line_split=re.split("\t",line)
+	track_id=line_split[0]
+	track_data=json.loads(line_split[1])
+	loudness_variance=track_data["segment_loudness_variance"]
+	variance_string="%010.6f" %variance
+	track_name=track_data["title"]
+	artist_name=track_data["artist_name"]
+	yield(variance_string,artist_name+" -- "+track_name)
 
 def reduce(word, counts):
 	pass
--- loudness.sh
+++ loudness.sh
@@ -1,4 +1,2 @@
-hadoop fs -rm filelist.txt
-hadoop fs -copyFromLocal filelist.txt filelist.txt
 hadoop fs -rmr loudness
-hadoop jar $SJAR -mapper "/usr/bin/python loudness.py map" -reducer /usr/bin/sort -input filelist.txt -output loudness -file "loudness.py" -file "common.py" -file "hdf5_getters.py"
+hadoop jar $SJAR -mapper "/usr/bin/python loudness.py map" -reducer /usr/bin/sort -input data -output loudness -file "loudness.py" -file "common.py"
