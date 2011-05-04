8c13d79071031412444ae5f8b143b68b31a0931e
Chris Gonzales <cgonzo86@gmail.com>
2011-05-04 12:36:35 -0400

    change fastest.sh to use input data

--- fastest.sh
+++ fastest.sh
@@ -1,2 +1,2 @@
 hadoop fs -rmr fastest
-hadoop jar $SJAR -mapper "/usr/bin/python fastest.py map" -reducer /usr/bin/sort -input filelist.txt -output fastest -file "fastest.py" -file "common.py"
+hadoop jar $SJAR -mapper "/usr/bin/python fastest.py map" -reducer /usr/bin/sort -input data -output fastest -file "fastest.py" -file "common.py"
