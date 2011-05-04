a9d68e78232596ddac398ca643b2a9a14b896c24
Chris Gonzales <cgonzo86@gmail.com>
2011-05-04 12:59:12 -0400

    cahnge loudness by year to use hadoop data

--- loudness_by_year.py
+++ loudness_by_year.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
 
+# gets average of loudness variance per year
+
 import common
-import hdf5_getters
+import json
+import re
 
 # input: file name
 # output: artist_id artist_name
 def map(line):
-	h5 = hdf5_getters.open_h5_file_read(line)
-	if(h5):
-		loudness=hdf5_getters.get_segments_loudness_start(h5,0)
-		if len(loudness)>0:
-			average_loudness=sum(loudness)/len(loudness)
-			variance=0
-			for value in loudness:
-				variance+=(value-average_loudness)**2
-			variance=variance/len(loudness)
-			year=hdf5_getters.get_year(h5,0)
-			yield(str(year),str(variance))
-		h5.close()
+	line_split=re.split("\t",line)
+	track_id=line_split[0]
+	track_data=json.loads(line_split[1])
+	loudness_variance=track_data["segment_loudness_variance"]
+	year=track_data["year"]
+	yield(str(year),str(loudness_variance))
 
 def reduce(word, counts):
 	average=0
--- loudness_by_year.sh
+++ loudness_by_year.sh
@@ -1,4 +1,2 @@
-hadoop fs -rm filelist.txt
-hadoop fs -copyFromLocal filelist.txt filelist.txt
 hadoop fs -rmr loudness_by_year
-hadoop jar $SJAR -mapper "/usr/bin/python loudness_by_year.py map" -reducer "/usr/bin/python loudness_by_year.py reduce" -input filelist.txt -output loudness_by_year -file "loudness_by_year.py" -file "common.py" -file "hdf5_getters.py"
+hadoop jar $SJAR -mapper "/usr/bin/python loudness_by_year.py map" -reducer "/usr/bin/python loudness_by_year.py reduce" -input data -output loudness_by_year -file "loudness_by_year.py" -file "common.py"
