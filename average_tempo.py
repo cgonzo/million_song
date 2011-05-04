eb0eb724b439255ff13957a895f2dfa0aa77edf0
Chris Gonzales <cgonzo86@gmail.com>
2011-05-04 11:18:12 -0400

    change average_tempo to use data in hadoop

--- average_tempo.py
+++ average_tempo.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
 import common
-import hdf5_getters
+import json
 
 # input: file name
 # output: artist_id artist_name
 def map(line):
-	h5 = hdf5_getters.open_h5_file_read(line)
-	if(h5):
-		tempo=hdf5_getters.get_tempo(h5,0)
-		if tempo>0.1:	# if tempo is unknown, set as 0
-			yield("1",str(tempo))
-		h5.close()
+	line_split=re.split("\t",line)
+	track_id=line_split[0]
+	track_data=json.loads(line_split[1])
+	tempo=track_data["tempo"]
+	if tempo>0.1:	# if tempo is unknown, set as 0
+		yield("1",str(tempo))
 
 def reduce(word, counts):
 	tempo_total=0
