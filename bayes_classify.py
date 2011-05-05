c5cb079fcfaa4bc34d734d5950d1714ba2b9f618
Chris Gonzales <cgonzo86@gmail.com>
2011-05-05 18:33:12 -0400

    fix probability calculation

--- bayes_classify.py
+++ bayes_classify.py
@@ -42,13 +42,11 @@ def map(line):
 					if(getattr(mean,'__iter__',False)):
 						for i in range (0,len(mean)):
 							if variance[i]>0:
-								# len(means) is to scale up so probability doesn't get infinitesimally small
-								probability=(count)*(1/math.sqrt(variance[i]*2*math.pi))*math.exp(-(track_value[i]-mean[i])**2/(2*variance[i]))
+								probability=(count/1000000)*(1/math.sqrt(variance[i]*2*math.pi))*math.exp(-(track_value[i]-mean[i])**2/(2*variance[i]))
 								probabilities.append(probability)
 					else:	
 						if variance>0:
-							# len(means) is to scale up so probability doesn't get infinitesimally small
-							probability=(count)*(1/math.sqrt(variance*2*math.pi))*math.exp(-(track_value-mean)**2/(2*variance))
+							probability=(count/1000000)*(1/math.sqrt(variance*2*math.pi))*math.exp(-(track_value-mean)**2/(2*variance))
 							probabilities.append(probability)
 							print str(track_value)+" "+str(mean)+" "+str(variance)+" "+str(probability)
 				term_probability=numpy.prod(numpy.array(probabilities))
