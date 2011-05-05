1eca6184dba4e053db9c104156afdbaa5f0b41b6
Chris Gonzales <cgonzo86@gmail.com>
2011-05-05 11:52:37 -0400

    use only artist's top term; don't limit number of categories reported

--- hottness.py
+++ hottness.py
@@ -20,9 +20,15 @@ def map(line):
 		if ((hottness>0) and (hottness<1.0)):
 			track_name=track_data["title"]
 			artist_name=track_data["artist_name"]
-			artist_terms=track_data["artist_terms"]
-			for term in artist_terms:
-				yield(term,str(hottness)+","+artist_name+" -- "+track_name)
+			terms=track_data["artist_terms"]
+			if len(terms)>0:
+				term_frequencies=track_data["artist_terms_freq"]
+				artist_name=track_data["artist_name"]
+				top_term=0
+				for i in range(len(terms)):
+					if(term_frequencies[i]>term_frequencies[top_term]):
+						top_term=i
+				yield(terms[top_term],str(hottness)+","+artist_name+" -- "+track_name)
 				
 def reduce(word, counts):
 	use_term=0
@@ -33,6 +39,7 @@ def reduce(word, counts):
 			use_term=1;
 		else:
 			artist_array.append(count_split)
+	use_term=1
 	if use_term==1:
 		sorted_array=sorted(artist_array, key=lambda artist_array: artist_array[0])
 		for i in range(1,11):
