daca915f5a6eb5de54a3ef6c4dd6d30afe879512
Chris Gonzales <cgonzo86@gmail.com>
2011-05-05 17:43:17 -0400

    initialize dictionaries for reducer, try 2

--- bayes_classify.py
+++ bayes_classify.py
@@ -61,6 +61,10 @@ def reduce(word, counts):
 	false_positives={}
 	false_negatives={}
 	correct={}
+	for key in classifier.keys():
+		correct[key]=0
+		false_positives[key]=0
+		false_negatives[key]=0
 	for count in counts:
 		count_split=re.split(",",count)
 		actual_term=count_split[0]
@@ -69,25 +73,10 @@ def reduce(word, counts):
 		if actual_term in classifier.keys():
 			# and classify into correct/incorrect buckets
 			if(top_probability==actual_term):
-				if actual_term in correct.keys():
-					correct[actual_term]+=1
-				else:
-					correct[actual_term]=1
-					false_positives[actual_term]=0
-					false_negatives[actual_term]=0
+				correct[actual_term]+=1
 			else:
-				if top_probability in correct.keys():
-					false_positives[top_probability]+=1
-				else:
-					correct[top_probability]=0
-					false_positives[top_probability]=1
-					false_negatives[top_probability]=0
-				if actual_term in correct.keys():
-					false_negatives[actual_term]+=1
-				else:
-					correct[actual_term]=0
-					false_positives[actual_term]=0
-					false_negatives[actual_term]=1
+				false_positives[top_probability]+=1
+				false_negatives[actual_term]+=1
 	for term in classifier.keys():
 		correct_percent=correct[term]/(correct[term]+false_negatives[term])
 		false_negative_percent=false_negatives[term]/(correct[term]+false_negatives[term])
