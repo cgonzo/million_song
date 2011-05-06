d7f9486c9e96aad31c0e707889076791d8a78eca
Chris Gonzales <cgonzo86@gmail.com>
2011-05-06 10:49:31 -0400

    artist_terms isn't a dict, so it has no keys

--- build_fisher.py
+++ build_fisher.py
@@ -24,7 +24,7 @@ def map(line):
 			for i in range(len(artist_terms)):
 				if(term_frequencies[i]>term_frequencies[top_term]):
 					top_term=i
-			for key in artist_terms.keys():
+			for key in artist_terms:
 				if key==artist_terms[top_term]:
 					match_string="1,"
 				else:
