d80526189855c78e68d65e1526480869442c6340
Chris Gonzales <cgonzo86@gmail.com>
2011-05-06 10:48:01 -0400

    move global variables to global

--- build_fisher.py
+++ build_fisher.py
@@ -11,12 +11,6 @@ from numpy import linalg
 # output: top term, data
 # but only if top term is in the top 10
 def map(line):
-	# create dictionary of test artists
-	artist_dict={}
-	f = open("artists_train.txt",'r')
-	for artist in f:
-		artist_dict[artist]=1
-	f.close()
 	line_split=re.split("\t",line)
 	track_id=line_split[0]
 	track_data=json.loads(line_split[1])
@@ -40,13 +34,6 @@ def map(line):
 
 def reduce(word, counts):
 	if len(counts)>10000:	# only want ones where there's a lot of data
-		interesting_data_names=["duration","num_bars","variance_bar_length","num_beats",
-			"variance_beats_length","danceability","end_of_fade_in","energy","key","loudness","mode",
-			"num_sections","variance_sections_length","num_segments","variance_segments_length",
-			"segment_loudness_max","segment_loudness_time","segment_loudness_mean",
-			"segment_loudness_variance","segment_pitches_mean","segment_pitches_variance",
-			"segment_timbres_mean","segment_timbres_variance","hottness","fade_out","num_tatums",
-			"variance_tatums_length","tempo","time_signature","year"]
 		# initialize storage
 		# data_for_key
 		# new row for each observation
@@ -81,4 +68,19 @@ def reduce(word, counts):
 		yield(word,json.dumps(v))
 
 if __name__ == "__main__":
+	global interesting_data_names
+	interesting_data_names=["duration","num_bars","variance_bar_length","num_beats",
+			"variance_beats_length","danceability","end_of_fade_in","energy","key","loudness","mode",
+			"num_sections","variance_sections_length","num_segments","variance_segments_length",
+			"segment_loudness_max","segment_loudness_time","segment_loudness_mean",
+			"segment_loudness_variance","segment_pitches_mean","segment_pitches_variance",
+			"segment_timbres_mean","segment_timbres_variance","hottness","fade_out","num_tatums",
+			"variance_tatums_length","tempo","time_signature","year"]
+	# create dictionary of test artists
+	global artist_dict
+	artist_dict={}
+	f = open("artists_train.txt",'r')
+	for artist in f:
+		artist_dict[artist.rstrip()]=1
+	f.close()
   common.main(map, reduce)
