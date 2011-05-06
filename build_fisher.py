36bd2c7e7ed23698bd645be81d4a60c3caf611db
Chris Gonzales <cgonzo86@gmail.com>
2011-05-06 10:54:07 -0400

    back to using artist_terms_sorted

--- build_fisher.py
+++ build_fisher.py
@@ -24,48 +24,49 @@ def map(line):
 			for i in range(len(artist_terms)):
 				if(term_frequencies[i]>term_frequencies[top_term]):
 					top_term=i
-			for key in artist_terms:
-				if key==artist_terms[top_term]:
-					match_string="1,"
-				else:
-					match_string="0,"
-				yield(key,match_string+line_split[1])
+			# if the top term is in one of the terms we care about, output it
+			if term_dict[artist_terms[top_term]]==1:
+				for key in term_dict.keys():
+					if key==artist_terms[top_term]:
+						match_string="1,"
+					else:
+						match_string="0,"
+					yield(key,match_string+line_split[1])
 				
 
 def reduce(word, counts):
-	if len(counts)>10000:	# only want ones where there's a lot of data
-		# initialize storage
-		# data_for_key
-		# new row for each observation
-		# column=data
-		data_for_key=[]
-		data_for_not_key=[]
-		data_for_key_mean=[]
-		data_for_not_key_mean=[]
-		data_for_key_covariance=[]
-		data_for_not_key_covariance=[]
-		# go through each song and store the data we want in interesting_data
-		for count in counts:
-			count_split=re.split(",",count)
-			track_data=json.loads(count_split[1])
-			if count_split[0]=="1":
-				data_for_key.append([])
-				for data_name in interesting_data_names:
-					data_for_key[len(data_for_key)-1].append(track_data[data_name])
-			else:
-				data_for_not_key.append([])
-				for data_name in interesting_data_names:
-					data_for_not_key[len(data_for_not_key)-1].append(track_data[data_name])
-		data_for_key_array=numpy.array(data_for_key)
-		data_for_not_key_array=numpy.array(data_for_not_key)
-		mean1=numpy.mean(data_for_key_array,axis=1)
-		mean2=numpy.mean(data_for_not_key_array,axis=1)
-		scatter1=numpy.shape(data_for_key_array)[0]*numpy.cov(data_for_key_array)
-		scatter2=numpy.shape(data_for_not_key_array)[0]*numpy.cov(data_for_not_key_array)
-		scatter_within=scatter1+scatter2
-		v=linalg.inv(scatter_within)*(mean1-mean2)
-		# output
-		yield(word,json.dumps(v))
+	# initialize storage
+	# data_for_key
+	# new row for each observation
+	# column=data
+	data_for_key=[]
+	data_for_not_key=[]
+	data_for_key_mean=[]
+	data_for_not_key_mean=[]
+	data_for_key_covariance=[]
+	data_for_not_key_covariance=[]
+	# go through each song and store the data we want in interesting_data
+	for count in counts:
+		count_split=re.split(",",count)
+		track_data=json.loads(count_split[1])
+		if count_split[0]=="1":
+			data_for_key.append([])
+			for data_name in interesting_data_names:
+				data_for_key[len(data_for_key)-1].append(track_data[data_name])
+		else:
+			data_for_not_key.append([])
+			for data_name in interesting_data_names:
+				data_for_not_key[len(data_for_not_key)-1].append(track_data[data_name])
+	data_for_key_array=numpy.array(data_for_key)
+	data_for_not_key_array=numpy.array(data_for_not_key)
+	mean1=numpy.mean(data_for_key_array,axis=1)
+	mean2=numpy.mean(data_for_not_key_array,axis=1)
+	scatter1=numpy.shape(data_for_key_array)[0]*numpy.cov(data_for_key_array)
+	scatter2=numpy.shape(data_for_not_key_array)[0]*numpy.cov(data_for_not_key_array)
+	scatter_within=scatter1+scatter2
+	v=linalg.inv(scatter_within)*(mean1-mean2)
+	# output
+	yield(word,json.dumps(v))
 
 if __name__ == "__main__":
 	global interesting_data_names
@@ -82,5 +83,10 @@ if __name__ == "__main__":
 	f = open("artists_train.txt",'r')
 	for artist in f:
 		artist_dict[artist.rstrip()]=1
+	f.close()
+	f = open("artist_by_most_popular_term_sorted.txt")
+	for term_line in f:
+		term_line_split=re.split("\t",term_line)
+		term_dict[term_line[1]]=1
 	f.close()
 	common.main(map, reduce)
