fb7a537363aabd68004c96adf1f114298f6b7a5d
Chris Gonzales <cgonzo86@gmail.com>
2011-05-06 14:03:28 -0400

    hottness was causing problem in inverting matrix; eliminate it for now

--- build_fisher.py
+++ build_fisher.py
@@ -92,12 +92,13 @@ def reduce(word, counts):
 
 if __name__ == "__main__":
 	global interesting_data_names
+	# removing hottness for now because it keeps us from inverting our matricies
 	interesting_data_names=["duration","num_bars","variance_bar_length","num_beats",
 			"variance_beats_length","danceability","end_of_fade_in","energy","key","loudness","mode",
 			"num_sections","variance_sections_length","num_segments","variance_segments_length",
 			"segment_loudness_max","segment_loudness_time","segment_loudness_mean",
 			"segment_loudness_variance","segment_pitches_mean","segment_pitches_variance",
-			"segment_timbres_mean","segment_timbres_variance","hottness","fade_out","num_tatums",
+			"segment_timbres_mean","segment_timbres_variance","fade_out","num_tatums",
 			"variance_tatums_length","tempo","time_signature","year"]
 	# create dictionary of test artists
 	global artist_dict
