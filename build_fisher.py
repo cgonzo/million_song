69e3345bb332089cae62be1a6e2d55273ff2672e
Chris Gonzales <cgonzo86@gmail.com>
2011-05-06 11:25:58 -0400

    add debug print

--- build_fisher.py
+++ build_fisher.py
@@ -57,16 +57,17 @@ def reduce(word, counts):
 			data_for_not_key.append([])
 			for data_name in interesting_data_names:
 				data_for_not_key[len(data_for_not_key)-1].append(track_data[data_name])
-	data_for_key_array=numpy.array(data_for_key)
-	data_for_not_key_array=numpy.array(data_for_not_key)
-	mean1=numpy.mean(data_for_key_array,axis=1)
-	mean2=numpy.mean(data_for_not_key_array,axis=1)
-	scatter1=numpy.shape(data_for_key_array)[0]*numpy.cov(data_for_key_array)
-	scatter2=numpy.shape(data_for_not_key_array)[0]*numpy.cov(data_for_not_key_array)
-	scatter_within=scatter1+scatter2
-	v=linalg.inv(scatter_within)*(mean1-mean2)
+	print data_for_key_array
+#	data_for_key_array=numpy.array(data_for_key)
+#	data_for_not_key_array=numpy.array(data_for_not_key)
+#	mean1=numpy.mean(data_for_key_array,axis=1)
+#	mean2=numpy.mean(data_for_not_key_array,axis=1)
+#	scatter1=numpy.shape(data_for_key_array)[0]*numpy.cov(data_for_key_array)
+#	scatter2=numpy.shape(data_for_not_key_array)[0]*numpy.cov(data_for_not_key_array)
+#	scatter_within=scatter1+scatter2
+#	v=linalg.inv(scatter_within)*(mean1-mean2)
 	# output
-	yield(word,json.dumps(v))
+#	yield(word,json.dumps(v))
 
 if __name__ == "__main__":
 	global interesting_data_names
