5dba4352de9fb826a4efef8e8f2ba4cfc662ef0f
Chris Gonzales <cgonzo86@gmail.com>
2011-05-07 18:16:42 -0400

    use data on hadoop

--- songs_per_artist.sh
+++ songs_per_artist.sh
@@ -1,4 +1,4 @@
 #export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-CDH3B4.jar
 hadoop fs -copyFromLocal filelist.txt filelist.txt
-hadoop jar $SJAR -mapper "/usr/bin/python songs_per_artist.py map" -reducer "/usr/bin/python songs_per_artist.py reduce" -input filelist.txt -output songs_per_artist -file "songs_per_artist.py" -file "common.py" -file "hdf5_getters.py"
+hadoop jar $SJAR -mapper "/usr/bin/python songs_per_artist.py map" -reducer "/usr/bin/python songs_per_artist.py reduce" -input data -output songs_per_artist -file "songs_per_artist.py" -file "common.py" -file "hdf5_getters.py"
 
