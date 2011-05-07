603fa986941bd12941a29d42067030fe782fa7d8
Chris Gonzales <cgonzo86@gmail.com>
2011-05-07 18:40:46 +0000

    cleaning up

--- bayes.sh
+++ bayes.sh
@@ -1,5 +1,5 @@
 hadoop fs -rmr build_bayes
-hadoop jar $SJAR -numReduceTasks 20 -mapper "/usr/bin/python build_bayes.py map" -reducer "/usr/bin/python build_bayes.py reduce" -input data -output build_bayes -file "build_bayes.py" -file "common.py" -file "artists_train.txt"
+hadoop jar $SJAR -numReduceTasks 20 -mapper "/usr/bin/python build_bayes.py map" -reducer "/usr/bin/python build_bayes.py reduce" -input data -output build_bayes -file "build_bayes.py" -file "common.py" -file "artists_train.txt" -file "artist_by_most_popular_term_sorted.txt"
 hadoop fs -rmr bayes_classify
 hadoop jar $SJAR -mapper "/usr/bin/python bayes_classify.py map" -reducer "/usr/bin/python bayes_classify.py reduce" -input data -output bayes_classify -file "bayes_classify.py" -file "common.py" -file "artists_train.txt" -cacheFile "hdfs://master:54310/user/root/build_bayes/#build_bayes"
 
--- classifiers.sh
+++
@@ -1,4 +0,0 @@
-hadoop fs -rm filelist.txt
-hadoop fs -copyFromLocal filelist.txt filelist.txt
-hadoop fs -rmr classifiers
-hadoop jar $SJAR -mapper "/usr/bin/python classifiers.py map" -reducer "/usr/bin/python classifiers.py reduce" -input filelist.txt -output classifiers -file "classifiers.py" -file "common.py" -file "hdf5_getters.py" -file "most_popular_terms_sorted.txt"
