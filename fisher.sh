d6ecb846217a52a0e42b17b3fdff3555bba92cbe
Chris Gonzales <cgonzo86@gmail.com>
2011-05-07 14:30:29 +0000

    increase timeout to 30 minutes

--- fisher.sh
+++ fisher.sh
@@ -1,5 +1,5 @@
 hadoop fs -rmr build_fisher
-hadoop jar $SJAR -numReduceTasks 10 -mapper "/usr/bin/python build_fisher.py map" -reducer "/usr/bin/python build_fisher.py reduce" -input data -output build_fisher -file "build_fisher.py" -file "common.py" -file "artists_train.txt" -file "artist_by_most_popular_term_sorted.txt"
+hadoop jar $SJAR -jobconf mapred.task.timeout=1800000 -numReduceTasks 40 -mapper "/usr/bin/python build_fisher.py map" -reducer "/usr/bin/python build_fisher.py reduce"  -input data -output build_fisher -file "build_fisher.py" -file "common.py" -file "artists_train.txt" -file "artist_by_most_popular_term_sorted.txt"
 hadoop fs -rmr fisher_classify
 hadoop jar $SJAR -mapper "/usr/bin/python fisher_classify.py map" -reducer "/usr/bin/python fisher_classify.py reduce" -input data -output fisher_classify -file "fisher_classify.py" -file "common.py" -file "artists_train.txt" -cacheFile "hdfs://master:54310/user/root/build_fisher/#build_fisher"
 
