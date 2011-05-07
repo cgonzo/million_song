1f2c892eea1a92f4a6afcf7b909053a7fdd6623f
Chris Gonzales <cgonzo86@gmail.com>
2011-05-07 14:09:40 -0400

    make fastest yield name and title rather than id

--- fastest.py
+++ fastest.py
@@ -10,10 +10,12 @@ def map(line):
 	line_split=re.split("\t",line)
 	track_id=line_split[0]
 	track_data=json.loads(line_split[1])
+	title=track_data["title"]
+	artist_name=track_data["artist_name"]
 	tempo=track_data["tempo"]
 	if tempo>0.1:	# if tempo is unknown, it is set as 0
 		tempo_string="%07.3f" %tempo
-		yield(tempo_string,track_id)
+		yield(tempo_string,artist_name+" -- "+title)
 
 def reduce(word, counts):
 	pass
