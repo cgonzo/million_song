77fea3fbb2544e636acf843b40bea5431c9e9852
Chris Gonzales <cgonzo86@gmail.com>
2011-05-07 18:19:46 -0400

    delete target directory

--- songs_per_artist.sh
+++ songs_per_artist.sh
@@ -1,4 +1,4 @@
 #export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-CDH3B4.jar
-hadoop fs -copyFromLocal filelist.txt filelist.txt
+hadoop fs -rmr songs_per_artist
 hadoop jar $SJAR -mapper "/usr/bin/python songs_per_artist.py map" -reducer "/usr/bin/python songs_per_artist.py reduce" -input data -output songs_per_artist -file "songs_per_artist.py" -file "common.py" -file "hdf5_getters.py"
 
