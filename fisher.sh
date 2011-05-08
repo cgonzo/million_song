5d7d3b52f6188682bbd2f670eba622584e18b0c5
Chris Gonzales <cgonzo86@gmail.com>
2011-05-08 15:21:30 -0400

    copy artists_train

--- fisher.sh
+++ fisher.sh
@@ -1,4 +1,4 @@
-cp /mnt/data/AdditionalFiles/artists_train.txt
+cp /mnt/data/AdditionalFiles/artists_train.txt .
 hadoop fs -rmr build_fisher
 hadoop jar $SJAR -jobconf mapred.task.timeout=1800000 -numReduceTasks 80 -mapper "/usr/bin/python build_fisher.py map" -reducer "/usr/bin/python build_fisher.py reduce"  -input data -output build_fisher -file "build_fisher.py" -file "common.py" -file "artists_train.txt" -file "artist_by_most_popular_term_sorted.txt"
 hadoop fs -rmr fisher_classify
